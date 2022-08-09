e06fe9c4bf8d8f63f2f25ddd79248063ffe40246
Seemamomin <seemamomin27@gmail.com>
2022-08-09 15:23:35 +0530

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +22,51 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+import "./style.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const itemList = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Java",
+    "Python",
+    "PHP",
+    "MYSQL",
+    "Oracle"
+  ];
+
+  const [filteredList, setFilteredList] = new useState(itemList);
+
+  const filterBySearch = (event) => {
+    // Access input value
+    const query = event.target.value;
+    // Create copy of item list
+    var updatedList = [...itemList];
+    // Include all elements which includes the search query
+    updatedList = updatedList.filter((item) => {
+      return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    // Trigger render with updated values
+    setFilteredList(updatedList);
+  };
+
+  return (
+    <div className="App">
+      <div className="search-header">
+        <div className="search-text">Search:</div>
+        <input id="search-box" onChange={filterBySearch} />
+      </div>
+      <div id="item-list">
+        <ol>
+          {filteredList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
